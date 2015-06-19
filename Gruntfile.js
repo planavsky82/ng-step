bf8071a414c8bbf0d796ea1469ca508c116ef719
Planavsky, Anthony (CORP) <Anthony.Planavsky@adp.com>
2015-06-19 02:26:29 -0400

    Setting up basic file structure.

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                files: {
+                    "build/app/framework/css/framework.css": "app/framework/less/framework.less",
+                    "build/app/framework/css/vendor.css": "app/framework/less/vendor.less"
+                }
+            }
+        },
+        watch: {
+            files: ["app/**/less/**.less","app/**","index.html"],
+            tasks: ["build"]
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        src: ['app/**','bower_components/**','index.html','!app/**/less/*.less','!app/**/less','!bower_components/**/less/*.less','!bower_components/**/less'],
+                        dest: 'build/'
+                    }
+                ]
+            }
+        },
+        clean: {
+            build: {
+                src: [ 'build' ]
+            }
+        },
+        open : {
+            dev : {
+                path: 'http://localhost:8080'
+                //app: 'Google Chrome'
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: 'build'
+                    //hostname: '*'
+                }
+            }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'app/**/*.js']
+        }
+        // server will run at http://localhost:8080
+        // source: http://www.sitepoint.com/writing-awesome-build-script-grunt/
+        // other tasks to look into: jshint, htmlhint, compile
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-htmlhint');
+
+    grunt.registerTask('default', ['build','connect','open','watch']);
+    grunt.registerTask(
+        'build',
+        'Compiles all of the assets and copies the files to the build directory.',
+        ['jshint', 'clean', 'copy', 'less']
+    );
+};
