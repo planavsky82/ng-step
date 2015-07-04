6844aca43b4e71d20eb05db7e2eb656ac7399a2c
Planavsky, Anthony (CORP) <Anthony.Planavsky@adp.com>
2015-07-04 02:26:38 -0400

    Adding sample pages in the demo.

--- demo/app.js
+++ demo/app.js
@@ -19,19 +19,31 @@
         id : 1,
         icon : 'icon1',
         desc : 'desc1',
-        url : ''
+        url : 'pages/page-1.html'
       },
       {
         id : 2,
         icon : 'icon2',
         desc : 'desc2',
-        url : ''
+        url : 'pages/page-2.html'
       },
       {
         id : 3,
         icon : 'icon3',
         desc : 'desc3',
-        url : ''
+        url : 'pages/page-3.html'
+      },
+      {
+        id : 4,
+        icon : 'icon4',
+        desc : 'desc4',
+        url : 'pages/page-4.html'
+      },
+      {
+        id : 5,
+        icon : 'icon5',
+        desc : 'desc5',
+        url : 'pages/page-5.html'
       }
     ];
 
